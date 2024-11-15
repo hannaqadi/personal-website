62143981efe4e8e49dc676549844e1b930b3160c
Hanna <hannaabdulalim@gmail.com>
2024-11-14 17:15:29 -0800

    added opacity background and bacground image

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './Homepage.module.css';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -64,6 +65,7 @@ const Homepage = () => {
           </div>
         </div>
         <div className={styles.rightContainer}>
+          <div className={styles.rightContainerGrid}>
           <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. <br></br>
             <br></br> Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
             <br></br> <br></br> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
@@ -106,6 +108,7 @@ const Homepage = () => {
                 <div className={styles.skill}>React Native</div>
                 <div className={styles.skill}>Typescript</div>
                 <div className={styles.skill}>CSS</div>
+              </div>
               </div>
             </div>
 
