fb2b08d8b6c867810d7e097ae67a6bef0c124183
Hanna <hannaabdulalim@gmail.com>
2024-11-26 17:26:38 -0800

    cleaning up. working on fixing scroll

--- src/Homepage.js
+++ src/Homepage.js
@@ -141,12 +141,12 @@ const Homepage = () => {
         </div>
         <div className={styles.rightContainer}>
           <div className={styles.rightContainerGrid} ref={aboutSection}>
-            {isMobile ? <h3>About</h3> : <></>}
+            {isMobile ? <h3 className={styles.headersMobile} >About</h3> : <></>}
             <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. <br></br>
               <br></br> Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
               <br></br> <br></br> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             </p>
-            {isMobile ? <h3>Experience</h3> : <></>}
+            {isMobile ? <h3 className={styles.headersMobile}>Experience</h3> : <></>}
             <div className={styles.rightExperienceContainer} ref={experienceSection}>
               <p> 2024 2024</p>
               <div>
